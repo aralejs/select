8b41b3ed1699b4768e062904ddbe40d984b84150
popomore <sakura9515@gmail.com>
2012-07-12 16:32:16 +0800

    add testcase

---
+++ tests/select-spec.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+    var $ = require('jquery');
+    var Select = require('../src/select');
+
+    describe('select', function() {
+
+        test('normal use', function() {
+            // add test code
+        });
+    });
+});
