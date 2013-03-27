6b2f36c9cc82993cc1cc90ec47a9b57aeb867bef
popomore <sakura9515@gmail.com>
2013-03-27 19:38:15 +0800

    change deps

--- dist/select-debug.js
+++ dist/select-debug.js
@@ -1,7 +1,7 @@
-define("arale/select/0.9.3/select-debug", [ "arale/overlay/0.9.13/overlay-debug", "$-debug", "arale/position/1.0.0/position-debug", "arale/iframe-shim/1.0.0/iframe-shim-debug", "arale/widget/1.0.2/widget-debug", "arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug", "arale/widget/1.0.2/templatable-debug", "gallery/handlebars/1.0.0/handlebars-debug" ], function(require, exports, module) {
-    var Overlay = require("arale/overlay/0.9.13/overlay-debug");
+define("arale/select/0.9.3/select-debug", [ "arale/overlay/1.0.1/overlay-debug", "$-debug", "arale/position/1.0.0/position-debug", "arale/iframe-shim/1.0.1/iframe-shim-debug", "arale/widget/1.0.3/widget-debug", "arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug", "arale/widget/1.0.3/templatable-debug", "gallery/handlebars/1.0.0/handlebars-debug" ], function(require, exports, module) {
+    var Overlay = require("arale/overlay/1.0.1/overlay-debug");
     var $ = require("$-debug");
-    var Templatable = require("arale/widget/1.0.2/templatable-debug");
+    var Templatable = require("arale/widget/1.0.3/templatable-debug");
     var template = '<div class="{{classPrefix}}">\n<ul class="{{classPrefix}}-content" data-role="content">\n{{#each select}}\n<li data-role="item" class="{{../classPrefix}}-item" data-value="{{value}}" data-defaultSelected="{{defaultSelected}}" data-selected="{{selected}}">{{{text}}}</li>\n{{/each}}\n</ul>\n</div>';
     var Select = Overlay.extend({
         Implements: Templatable,
