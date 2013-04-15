90859b335ff5b141f527e1183dd13cc6bec33876
popomore <sakura9515@gmail.com>
2013-04-15 21:17:57 +0800

    build spm2

--- dist/select-debug.js
+++ dist/select-debug.js
@@ -1,8 +1,8 @@
-define("arale/select/0.9.3/select-debug", [ "arale/overlay/1.0.1/overlay-debug", "$-debug", "arale/position/1.0.0/position-debug", "arale/iframe-shim/1.0.1/iframe-shim-debug", "arale/widget/1.0.3/widget-debug", "arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug", "arale/widget/1.0.3/templatable-debug", "gallery/handlebars/1.0.0/handlebars-debug" ], function(require, exports, module) {
+define("arale/select/0.9.3/select-debug", [ "arale/overlay/1.0.1/overlay-debug", "$-debug", "arale/position/1.0.0/position-debug", "arale/iframe-shim/1.0.1/iframe-shim-debug", "arale/widget/1.0.3/widget-debug", "arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug", "arale/widget/1.0.3/templatable-debug", "gallery/handlebars/1.0.0/handlebars-debug", "./select-debug.tpl" ], function(require, exports, module) {
     var Overlay = require("arale/overlay/1.0.1/overlay-debug");
     var $ = require("$-debug");
     var Templatable = require("arale/widget/1.0.3/templatable-debug");
-    var template = '<div class="{{classPrefix}}">\n<ul class="{{classPrefix}}-content" data-role="content">\n{{#each select}}\n<li data-role="item" class="{{../classPrefix}}-item" data-value="{{value}}" data-defaultSelected="{{defaultSelected}}" data-selected="{{selected}}">{{{text}}}</li>\n{{/each}}\n</ul>\n</div>';
+    var template = require("./select-debug.tpl");
     var Select = Overlay.extend({
         Implements: Templatable,
         attrs: {
@@ -316,3 +316,5 @@ define("arale/select/0.9.3/select-debug", [ "arale/overlay/1.0.1/overlay-debug",
         return index;
     }
 });
+
+define("arale/select/0.9.3/select-debug.tpl", [], '<div class="{{classPrefix}}">\n    <ul class="{{classPrefix}}-content" data-role="content">\n        {{#each select}}\n        <li data-role="item" class="{{../classPrefix}}-item" data-value="{{value}}" data-defaultSelected="{{defaultSelected}}" data-selected="{{selected}}">{{{text}}}</li>\n        {{/each}}\n    </ul>\n</div>\n');
